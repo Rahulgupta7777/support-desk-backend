e91836985c641a8d97740ddea77fceb69e7ba236
Rahul Gupta <rahulgupta193246@gmail.com>
2025-12-31 13:33:04 +0530

    second commit

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose")
 const routes = require("./routes/books")
+const fs = require('fs')
 
 const app = express();
 app.use(express.json())
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/",(req,res)=>{
+app.use("/",async(req,res)=>{
+    console.log(req.statusCode)
+    await fs.writeFile("data.json", JSON.stringify({
+                statusCode: res.statusCode,
+                url: req.url,
+                method: req.method,
+                dirname: __dirname,
+                time: new Date().toISOString()
+            }), "utf-8");
     res.send("Hello World!")
 })
 
-app.use("/Books",routes)
+app.use("/books",routes)
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
--- routes/books.js
+++ routes/books.js
@@ -1,36 +1,63 @@
 const express = require("express")
 const router = express.Router()
 const Book = require("../models/books")
+const fs = require('fs/promises')
 
+router.use(express.json())
 
-
-router.get('/Books', async (req, res) => {
+router.get('/books', async (req, res) => {
     try {
+    await fs.writeFile("data.json", JSON.stringify({
+    statusCode: res.statusCode,
+    url: req.url,
+    method: req.method,
+    dirname: __dirname,
+    time: new Date().toISOString()
+    }), "utf-8");
         const books = await Book.find();
-        res.json(books);
+        res.json(books)
     } catch(err) {
         console.error(err.message);
         res.send(400).send('Server Error');
     }
 });
 
-router.get('/Books/:id', async (req, res) => {
+router.get('/books/:id', async (req, res) => {
     try {
         const item = await Book.findById(req.params.id);
-        res.json(item);
+        const data = await fs.readFile('data.json', 'utf8');
+        const dataBooks = JSON.parse(data);
+         await fs.writeFile("data.json", JSON.stringify({
+            statusCode: res.statusCode,
+            id:req.params.id,
+            url: req.url,
+            method: req.method,
+            dirname: __dirname,
+            time: new Date().toISOString()
+        }), "utf-8");
+        res.json(dataBooks);
     } catch(err) {
         console.error(err.message);
         res.send(400).send('Server Error');
     }
 });
 
-router.post('/Books/:id', async (req, res) => {
+router.post('/books/:id', async (req, res) => {
     try {
+        
         const { name, author } = await req.body;
         const newBook = new Book({
             name,
             author
           });
+           await fs.writeFile("data.json", JSON.stringify({
+            statusCode: res.statusCode,
+              id:req.params.id,
+            url: req.url,
+            method: req.method,
+            dirname: __dirname,
+            time: new Date().toISOString()
+        }), "utf-8");
 
           await Book.save()
           res.json('book added!',newBook)    
@@ -41,22 +68,39 @@ router.post('/Books/:id', async (req, res) => {
 });
 
 
-router.delete('/Books/:id', async (req, res) => {
+router.delete('/books/:id', async (req, res) => {
     try {
          await Book.findByIdAndDelete(req.params.id);
         res.json('book deleted.');
+         await fs.writeFile("data.json", JSON.stringify({
+            statusCode: res.statusCode,
+              id:req.params.id,
+            url: req.url,
+            method: req.method,
+            dirname: __dirname,
+            time: new Date().toISOString()
+        }), "utf-8");
 
         } catch(err) {
           res.status(400).json('Error: ' + err);
      }
   });
 
-  router.put('/Books/:id', async (req, res) => {
+  router.put('/books/:id', async (req, res) => {
     try {
          await Book.findByIdAndUpdate(req.params.id, {
             name: req.body.name,
             author: req.body.author
+            
         });
+         await fs.writeFile("data.json", JSON.stringify({
+            statusCode: res.statusCode,
+              id:req.params.id,
+            url: req.url,
+            method: req.method,
+            dirname: __dirname,
+            time: new Date().toISOString()
+        }), "utf-8");
         res.json('book updated!');
         
     } catch(err) {
